1f683bfeb6d94bc70896ef0a72e3a4de83a0fa6e
grace-mao23 <gmao00@stuy.edu>
2019-04-01 09:53:32 -0400

    Method created to determine number of passes

--- Radix.java
+++ Radix.java
@@ -1,7 +1,21 @@
 public class Radix {
 
   public void radixsort(int[] data){
-    
+    int passes = maxL(data);
+    for (int i = 1; i <= passes; i++) {
+      
+    }
+  }
+
+  // returns number of passes necessary
+  private int maxL(int[] data) {
+    int result = data[0];
+    for (int i : data) {
+      if (i > result) {
+        result = i;
+      }
+    }
+    return (""+result).length();
   }
 
 }
