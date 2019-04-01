39c387d30ef299ee89a5149c1a96a64a4280da96
grace-mao23 <gmao00@stuy.edu>
2019-04-01 10:16:51 -0400

    Isolated digit

--- Radix.java
+++ Radix.java
@@ -1,14 +1,26 @@
 public class Radix {
 
-  public void radixsort(int[] data){
+//  @SuppressWarnings("unchecked")
+  public static void radixsort(int[] data){
     int passes = maxL(data);
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+    MyLinkedList<Integer> current = new MyLinkedList<Integer>();
+    for (int i : data) {
+      current.add(i);
+    }
     for (int i = 1; i <= passes; i++) {
-      
+      for (int x = 0; x < data.length; x++) {
+        int num = current.remove(0);
+        int index = (int)(num / (Math.pow(10,i-1))) % 10;
+      }
     }
   }
 
   // returns number of passes necessary
-  private int maxL(int[] data) {
+  private static int maxL(int[] data) {
+    if (data.length == 0) {
+      return 0;
+    }
     int result = data[0];
     for (int i : data) {
       if (i > result) {
