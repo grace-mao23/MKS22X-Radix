5489e0d48258fcb89bbb629cc156e57c1e6c17ce
grace-mao23 <gmao00@stuy.edu>
2019-04-02 10:15:09 -0400

    Optimizing radix

--- Radix.java
+++ Radix.java
@@ -3,35 +3,53 @@ import java.util.*;
 
 public class Radix {
 
-//  @SuppressWarnings("unchecked")
   public static void radixsort(int[] data){
     int passes = maxL(data);
     MyLinkedList[] buckets = new MyLinkedList[20];
+    MyLinkedList<Integer> temp = new MyLinkedList<Integer>();
     for (int i = 0; i < buckets.length; i++) {
       buckets[i] = new MyLinkedList<Integer>();
     }
     for (int i = 1; i <= passes; i++) {
-    //  System.out.println(current.toString());
-      for (int x = 0; x < data.length; x++) {
-      //  System.out.println(x);
-        int num = data[x];
-        int index = (int)(num / (Math.pow(10,i-1))) % 10;
-        buckets[index+9].add(num);
+      if (i == 1) {
+        for (int x = 0; x < data.length; x++) {
+          int num = data[x];
+          int index = (int)(num / (Math.pow(10,i-1))) % 10;
+          buckets[index+9].add(num);
+        }
+    //    System.out.println(Arrays.toString(buckets));
+        temp.clear();
+        for (int y = 0; y < 20; y++) {
+        //  System.out.println("1: "+temp.toString());
+          temp.extend(buckets[y]);
+        //  System.out.println("T: "+temp.toString());
+        }
+      /*  for (int z = 0; z < data.length; z++) {
+          data[z] = temp.remove(0);
+        } */
+        clearB(buckets);
+      //  System.out.println(temp.toString());
+      //  System.out.println("-----------\n"+Arrays.toString(buckets));
+      } else {
+        for (int x = 0; x < data.length; x++) {
+          int num = temp.remove(0);
+          int index = (int)(num / (Math.pow(10,i-1))) % 10;
+          buckets[index+9].add(num);
+        }
       //  System.out.println(Arrays.toString(buckets));
+        temp.clear();
+        for (int y = 0; y < 20; y++) {
+      //    System.out.println("1: "+temp.toString());
+          temp.extend(buckets[y]);
+      //    System.out.println("T: "+temp.toString());
+        }
+      /*  for (int z = 0; z < data.length; z++) {
+          data[z] = temp.remove(0);
+        } */
+        clearB(buckets);
+      //  System.out.println(temp.toString());
+      //  System.out.println("-----------\n"+Arrays.toString(buckets));
       }
-      MyLinkedList<Integer> temp = buckets[0];
-      for (int y = 1; y < 20; y++) {
-        //System.out.println("1: "+temp.toString());
-        //System.out.println(buckets[y].toString());
-        temp.extend(buckets[y]);
-        //System.out.println("2: "+temp.toString());
-      }
-      //System.out.println(temp.toString());
-      for (int z = 0; z < data.length; z++) {
-        data[z] = temp.remove(0);
-      }
-      clearB(buckets);
-    //  System.out.println(Arrays.toString(data));
     }
   //  System.out.println(Arrays.toString(data));
   }
@@ -57,13 +75,13 @@ public class Radix {
   }
 
   public static void main(String[] args) {
-    /*int[] test = new int[] { 62, 21, 32, 13, 41, 24, 25 };
-    Radix.radixsort(test);
-    System.out.println(Arrays.toString(test));
+  //  int[] test = new int[] { 62, 21, 32, 13, 41, 24, 25 };
+    //Radix.radixsort(test);
+  //  System.out.println(Arrays.toString(test));
     int[] test2 = new int[] { 12, -13, 21,-4, 43, -32, 0, -1 };
     Radix.radixsort(test2);
-    System.out.println(Arrays.toString(test2)); */
-      System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    System.out.println(Arrays.toString(test2));
+  /*    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
       int[]MAX_LIST = {1000000000,500,10};
       for(int MAX : MAX_LIST){
         for(int size = 31250; size < 2000001; size*=2){
@@ -94,7 +112,7 @@ public class Radix {
           System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
         }
         System.out.println();
-      }
+      } */
     }
 
 }
